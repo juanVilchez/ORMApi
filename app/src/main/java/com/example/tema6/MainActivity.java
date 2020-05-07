dfef46fd0a7b9133dd404a813016b0e3e355620b
aliianza12 <juanv9664@gmail.com>
2020-05-07 14:46:32 -0500

    Se añade un RecyclerView para la visualización de datos estáticos. añadimos permisos y librerias en el grable

--- app/src/main/java/com/example/tema6/MainActivity.java
+++ app/src/main/java/com/example/tema6/MainActivity.java
@@ -1,14 +1,34 @@
 package com.example.tema6;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Vector;
 
+public class MainActivity extends AppCompatActivity {
+    private RecyclerView recyclerView;
+    private RecyclerView.LayoutManager layoutManager;
+    private MiNuevoAdaptador adaptador;
+    private Vector<String> misdatos;
+    public Vector<String> valor;
+    private String res;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        recyclerView = findViewById(R.id.recycler_view);
+        misdatos = new Vector<String>();
+        misdatos.add("123000 Wilson Callisaya");
+        misdatos.add("123000 Pepito Domingez");
+        adaptador = new MiNuevoAdaptador(this,
+                misdatos);
+/* adaptador = new MiNuevoAdaptador(this,
+ListaClientes(8));*/
+        recyclerView.setAdapter(adaptador);
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
     }
 }
